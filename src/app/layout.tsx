6f2b3a307588c2c9045e38f9a50743dc9911416e
Dóra Tokai <tokaidora88@gmail.com>
2024-04-26 14:27:13 +0300

    Add footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { Lato } from 'next/font/google';
 import './globals.css';
 import Header from '@/components/Header';
+import Footer from '@/components/Footer';
 
 const lato = Lato({ subsets: ['latin'], weight: ['400', '700'] });
 
@@ -20,6 +21,7 @@ export default function RootLayout({
       <body className={lato.className}>
         <Header />
         {children}
+        <Footer />
       </body>
     </html>
   );
