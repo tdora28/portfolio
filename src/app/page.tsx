236502da263af52c5e03eb822c7de5b8b2c27e44
Dóra Tokai <tokaidora88@gmail.com>
2024-04-26 15:55:14 +0300

    Make landing responsive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,11 +8,11 @@ export default function Home() {
     <>
       <main className="container">
         {/* Title */}
-        <h1 className={`${fontDisplay} text-4xl text-center shine-effect`}>
-          Web Dev <span className="block text-9xl">Portfolio</span>
+        <h1 className={`${fontDisplay} text-3xl sm:text-4xl text-center shine-effect`}>
+          Web Dev <span className="block text-6xl sm:text-9xl">Portfolio</span>
         </h1>
         {/* Big nav */}
-        <nav className={`${fontDisplay} grid grid-cols-3 gap-10 w-full max-w-[800px] h-[40vh] mt-[15vh] mx-auto text-4xl text-center overflow-hidden`}>
+        <nav className={`${fontDisplay} grid sm:grid-cols-3 gap-5 sm:gap-10 w-full max-w-[800px] sm:h-[40vh] mt-[15vh] mx-auto text-4xl text-center overflow-hidden`}>
           <Link href="/about" className="grow py-8 bg-white clr-dark move-up">
             About
           </Link>
