0e9e4950362fce89e18f70a14fd6a6adfb92d842
Dóra Tokai <tokaidora88@gmail.com>
2024-04-29 12:00:13 +0300

    Make ProjectCard dynamic

--- src/app/work/page.tsx
+++ src/app/work/page.tsx
@@ -3,19 +3,68 @@ import { fontDisplay } from '@/utilities/font';
 import ProjectCard from '@/components/ProjectCard';
 
 const WorkPage = () => {
+  const projects = [
+    {
+      id: 1,
+      imageUrl: '/example-project.jpg',
+      title: 'Example Project',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam vehicula, purus nec lacinia fermentum, nunc nunc ultricies turpis, vel volutpat diam justo eu purus.',
+      liveDemoUrl: 'https://github.com/tdora28/',
+      codeUrl: 'https://github.com/tdora28/',
+    },
+    {
+      id: 2,
+      imageUrl: '/example-project.jpg',
+      title: 'Example Project',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam vehicula, purus nec lacinia fermentum, nunc nunc ultricies turpis, vel volutpat diam justo eu purus.',
+      liveDemoUrl: 'https://github.com/tdora28/',
+      codeUrl: 'https://github.com/tdora28/',
+    },
+    {
+      id: 3,
+      imageUrl: '/example-project.jpg',
+      title: 'Example Project',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam vehicula, purus nec lacinia fermentum, nunc nunc ultricies turpis, vel volutpat diam justo eu purus.',
+      liveDemoUrl: 'https://github.com/tdora28/',
+      codeUrl: 'https://github.com/tdora28/',
+    },
+    {
+      id: 4,
+      imageUrl: '/example-project.jpg',
+      title: 'Example Project',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam vehicula, purus nec lacinia fermentum, nunc nunc ultricies turpis, vel volutpat diam justo eu purus.',
+      liveDemoUrl: 'https://github.com/tdora28/',
+      codeUrl: 'https://github.com/tdora28/',
+    },
+    {
+      id: 5,
+      imageUrl: '/example-project.jpg',
+      title: 'Example Project',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam vehicula, purus nec lacinia fermentum, nunc nunc ultricies turpis, vel volutpat diam justo eu purus.',
+      liveDemoUrl: 'https://github.com/tdora28/',
+      codeUrl: 'https://github.com/tdora28/',
+    },
+    {
+      id: 6,
+      imageUrl: '/example-project.jpg',
+      title: 'Example Project',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam vehicula, purus nec lacinia fermentum, nunc nunc ultricies turpis, vel volutpat diam justo eu purus.',
+      liveDemoUrl: 'https://github.com/tdora28/',
+      codeUrl: 'https://github.com/tdora28/',
+    },
+  ];
+
   return (
     <>
       <main className="container">
         <h2 className={`${fontDisplay} text-5xl sm:text-6xl py-20`}>Project showcase</h2>
 
         {/* Project Cards */}
+        {/* imageUrl, title, description, liveDemoUrl, codeUrl */}
         <section className="flex flex-wrap justify-evenly gap-10">
-          <ProjectCard />
-          <ProjectCard />
-          <ProjectCard />
-          <ProjectCard />
-          <ProjectCard />
-          <ProjectCard />
+          {projects.map((project) => (
+            <ProjectCard key={project.id} imageUrl={project.imageUrl} title={project.title} description={project.description} liveDemoUrl={project.liveDemoUrl} codeUrl={project.codeUrl} />
+          ))}
         </section>
       </main>
 
--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -3,22 +3,28 @@ import Image from 'next/image';
 import { fontDisplay } from '@/utilities/font';
 import Link from 'next/link';
 
-type Props = {};
+type ProjectCardProps = {
+  imageUrl: string;
+  title: string;
+  description: string;
+  liveDemoUrl: string;
+  codeUrl: string;
+};
 
-const ProjectCard = (props: Props) => {
+const ProjectCard: React.FC<ProjectCardProps> = ({ imageUrl, title, description, liveDemoUrl, codeUrl }) => {
   return (
     <div className="max-w-[300px] border-b-2 border-white ">
       <div>
-        <Image src="/example-project.jpg" alt="Project Image" width={800} height={500} />
+        <Image src={imageUrl} alt="Project Image" width={800} height={500} />
       </div>
       <div className="py-8">
-        <h3 className={`${fontDisplay} text-3xl`}>Example Project</h3>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam vehicula, purus nec lacinia fermentum, nunc nunc ultricies turpis, vel volutpat diam justo eu purus.</p>
+        <h3 className={`${fontDisplay} text-3xl`}>{title}</h3>
+        <p>{description}</p>
         <div className={`${fontDisplay} flex gap-4 text-xl`}>
-          <Link href="https://github.com/tdora28/" className="bg-white text-black py-2 px-4">
+          <Link href={liveDemoUrl} className="bg-white text-black py-2 px-4">
             Live demo
           </Link>
-          <Link href="https://github.com/tdora28/" className="bg-white text-black py-2 px-4">
+          <Link href={codeUrl} className="bg-white text-black py-2 px-4">
             Code
           </Link>
         </div>
